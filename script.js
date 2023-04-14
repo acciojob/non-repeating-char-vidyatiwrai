a130bbd2532c932cab1d4d2035a85d2af9b8b70a
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-14 16:43:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+
+function firstNonRepeatedCharacter(string) {
+    return string.split('').filter(function (character, index, obj) {
+        return obj.indexOf(character) === obj.lastIndexOf(character);
+    }).shift();
+}
+
+console.log(firstNonRepeatedCharacter('aabcbd'));
