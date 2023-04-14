e279fdd2174e335f0aac890ea01a2b08892594c8
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-14 17:09:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //your JS code here. If required.
-
+var string = "aabbcedd";
 function firstNonRepeatedCharacter(string) {
+   
 for (let i = 0; i < string.length; i++) {
 let character = string.charAt(i);
 if (string.indexOf(character) === i && string.indexOf(character, i + 1) === -1) {
-return character;
+console.log(character);
 }
 }
-return null;
+
 }
+firstNonRepeatedCharacter("aabbcdd");
 
 
