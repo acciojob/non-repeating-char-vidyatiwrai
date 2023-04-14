b863f93abc8ca0542d5c8985cd59e401bd7d5a2d
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-14 22:00:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ let string = prompt("Enter a string:");
 function firstNonRepeatedCharacter(string) {
    
 for (let i = 0; i < string.length; i++) {
-let character = string.charAt(i);
-if (string.indexOf(character) === i && string.indexOf(character, i + 1) === -1) {
-console.log(character);
+let result = string.charAt(i);
+if (string.indexOf(result) === i && string.indexOf(result, i + 1) === -1) {
+// console.log(character);
 }
 }
+// return  str
 
+if (result) {
+alert(The first non-repeated character is "${result}".);
+} else {
+alert("There's no non-repeated character in the string.");
+}
 }
 firstNonRepeatedCharacter(string); 
+
 
 
